e1bc8f003cfa97b4a5e48aa4b3cceb45deb5e6ed
Luke Vanden Bosch <luke_vanden_bosch@hotmail.com>
2017-12-01 14:42:56 -0500

    Added standard header and footer to the productpag

--- viewproducts.php
+++ viewproducts.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="theme.css">
+    <title>Smart Tech Supplies</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="theme.css">
+    <style></style>
 </head>
 <body>
+
+<div class="container">
+<?php include "header.php";?>
+
+
+<!--Main Navigation Menu -->
+<?php include 'mainnaviation.php';?>
+
+<!--Main content Menu -->
+
 <table class="ItemTable">
 <tr>
 	<th>Picture</th>
@@ -12,5 +26,9 @@
 	<th>Quantity Instock</th>
 </tr>
 </table>
+<!--footer -->
+<?php include 'footer.php';?>
+</div>
+
 </body>
 </html>
