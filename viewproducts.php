72fb6af2eecc7ea621e1ad3651ca30062a3c40a5
Luke Vanden Bosch <luke_vanden_bosch@hotmail.com>
2017-12-01 14:39:44 -0500

    Adding an item table

---
+++ viewproducts.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="theme.css">
+</head>
+<body>
+<table class="ItemTable">
+<tr>
+	<th>Picture</th>
+	<th>Name</th>
+	<th>Price</th>
+	<th>Quantity Instock</th>
+</tr>
+</table>
+</body>
+</html>
