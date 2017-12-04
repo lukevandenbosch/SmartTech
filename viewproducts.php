87ddccd13f047b3169e11052e118ba0e59c9ab57
Luke Vanden Bosch <luke_vanden_bosch@hotmail.com>
2017-12-04 13:11:29 -0500

    adding in objects to table

--- viewproducts.php
+++ viewproducts.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="theme.css">
+	<link rel="stylesheet" type="text/css" href="table.css">
     <style></style>
 </head>
 <body>
@@ -24,6 +25,66 @@
 	<th>Name</th>
 	<th>Price</th>
 	<th>Quantity Instock</th>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/dell1.jpg" class="picture1"></td>
+	<td>Dell Inspiron 15 5000 Gaming (AMD) Laptop 15.6 inch"</td>
+	<td>$899.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/msi1.jpg" class="picture1"></td>
+	<td>MSI GL62M 7REX-1067 15.6"</td>
+	<td>$1,499.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/surface.jpg" class="picture1"></td>
+	<td>Surface Laptop</td>
+	<td>$1,199.99</td>
+	<td>35</td>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
+</tr>
+<tr>
+	<td><img src="Pictures/AlienWare.jpg" class="picture1"></td>
+	<td>Alienware 15 R3 AW15R3-7390SLV 15.6"</td>
+	<td>$3,199.99</td>
+	<td>50</td>
 </tr>
 </table>
 <!--footer -->
