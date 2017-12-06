e45f8580e60eeb4acced4d9d5cf7ea2c80e79579
Luke Vanden Bosch <luke_vanden_bosch@hotmail.com>
2017-12-05 21:51:42 -0500

    Completed table for part 1

--- viewproducts.php
+++ viewproducts.php
@@ -36,31 +36,31 @@
 	<td><img src="Pictures/dell1.jpg" class="picture1"></td>
 	<td>Dell Inspiron 15 5000 Gaming (AMD) Laptop 15.6 inch"</td>
 	<td>$899.99</td>
-	<td>50</td>
+	<td>92</td>
 </tr>
 <tr>
 	<td><img src="Pictures/msi1.jpg" class="picture1"></td>
 	<td>MSI GL62M 7REX-1067 15.6"</td>
 	<td>$1,499.99</td>
-	<td>50</td>
+	<td>84</td>
 </tr>
 <tr>
 	<td><img src="Pictures/dell2.jpg" class="picture1"></td>
 	<td>Dell Inspiron 15 7000 Gaming Series w/ Intel Core</td>
 	<td>$1,599.99</td>
-	<td>50</td>
+	<td>88</td>
 </tr>
 <tr>
 	<td><img src="Pictures/msi2.jpg" class="picture1"></td>
 	<td>MSI GS Series GS63VR Stealth Pro-422 15.6" Intel Core i7 6th Gen </td>
 	<td>$1,999.99</td>
-	<td>50</td>
+	<td>78</td>
 </tr>
 <tr>
 	<td><img src="Pictures/acer1.jpg" class="picture1"></td>
 	<td>Acer Acer Aspire Nitro An515-51-58Lk 15.6"</td>
 	<td>$898.00</td>
-	<td>50</td>
+	<td>42</td>
 </tr>
 <tr>
 	<td><img src="Pictures/surface.jpg" class="picture1"></td>
@@ -72,21 +72,22 @@
 	<td><img src="Pictures/asus1.jpg" class="picture1"></td>
 	<td>ASUS ROG Zephyrus GX501VS-XS71 15.6"</td>
 	<td>$2,899.99</td>
-	<td>50</td>
+	<td>37</td>
 </tr>
 <tr>
 	<td><img src="Pictures/asus2.jpg" class="picture1"></td>
 	<td>ASUS ROG GL753VD-DS71 17.3" Intel Core i7 7th Gen</td>
 	<td>$1,204.94</td>
-	<td>50</td>
+	<td>15</td>
 </tr>
 <tr>
 	<td><img src="Pictures/asus3.jpg" class="picture1"></td>
 	<td>ASUS ROG GL502VS-Q72S-CB 15.6" Intel Core i7 6th Gen</td>
 	<td>$2,599.00</td>
-	<td>50</td>
+	<td>56</td>
 </tr>
 </table>
+
 <!--footer -->
 <?php include 'footer.php';?>
 </div>
