907c9235a4ba1093f77525e017464059864020c5
peter.robinson <peter.robinson.on@gmail.com>
2017-12-07 15:32:49 -0500

    Added Product categories to front page.

--- header.php
+++ header.php
@@ -4,6 +4,7 @@ print '
 <header id="masthead" class="site-header" role="banner">
     <div class="container">
         <div class="site-branding">
+        <a href="http://lottery-insights.ingenioussoftwarelabs.com/" class="custom-logo-link" rel="home" itemprop="url"><img src="images/logo2.png" class="custom-logo" alt="Lottery-Insights" itemprop="logo" srcset="http://lottery-insights.ingenioussoftwarelabs.com/wp-content/uploads/2017/12/cropped-t1-1.png 307w, http://lottery-insights.ingenioussoftwarelabs.com/wp-content/uploads/2017/12/cropped-t1-1-300x152.png 300w" sizes="(max-width: 307px) 100vw, 307px" width="307" height="156"></a>
             <div id="site-identity">
                 <h1 class="site-title"><a href="index.php" rel="home">Smart Tech Supplies</a></h1>
                 <p class="site-description">High Quality and Affordable Supplies</p>
