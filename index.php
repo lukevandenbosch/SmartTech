12419c1e7726ee4ce0fb84658127c0c52dff3bee
peter.robinson <peter.robinson.on@gmail.com>
2017-12-07 11:50:35 -0500

    Merged Picture slide with Video on home page.

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+//echo "This is our Main Page";
+
+?>
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
@@ -21,13 +26,39 @@
 <!--Main content Menu -->
 
     <section id="main-content">
-        <iframe width="854" height="480" src="https://www.youtube.com/embed/3Wf-aehKbuk" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+        <img src ="appliances.jpg" alt="template"  style="display: block;margin: auto;width: 40%;"/>
+        <hr>
+        <h2>Features Products</h2>
+        <hr>
+        <div class="w3-content w3-section">
+            <img class="mySlides" src="images/pc1.jpg" style="width:540px" alt="1">
+            <img class="mySlides" src="images/pc2.jpg" style="width:540px" alt="2">
+            <img class="mySlides" src="images/printer1.jpg" style="width:540px" alt="3">
+            <img class="mySlides" src="images/appliances.jpg" style="width:540px" alt="4">
+        </div>
+    </section>
     </section>
 
 
 <!--footer -->
 <?php include 'footer.php';?>
 </div>
+<script>
+    var myIndex = 0;
+    carousel();
 
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}
+        x[myIndex-1].style.display = "block";
+        setTimeout(carousel, 5000); // Change image every 2 seconds
+    }
+</script>
 </body>
 </html>
+
