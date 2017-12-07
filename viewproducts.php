535665fa7f6bd8935199a64fe2e3d5622632946d
peter.robinson <peter.robinson.on@gmail.com>
2017-12-07 13:26:17 -0500

    Apply table styling

--- viewproducts.php
+++ viewproducts.php
@@ -19,26 +19,33 @@
 
 <!--Main content Menu -->
 
-<table class="ItemTable">
+<table class="ItemTable" border="0">
 <tr>
 	<td><img src="images/AlienWare.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
-    Price:</br>
-    Quantity Instock:</br>
-        &nbsp</td>
-    <td class="td-element">Alienware 15 R3 AW15R3-7390SLV 15.6"<br>
-    $3,199.99</br>
-	50</br>
-        <a href="images/Alienware.jpg">Large</a> |
-        <a href="#">Remove Product</a></td>
+    <td class="td-align-right">
+        <p>
+            Product:</br>
+            Price:</br>
+            Quantity Instock:</br>
+            &nbsp
+        </p>
+    </td>
+    <td class="td-align-left">
+        <p>
+            Alienware 15 R3 AW15R3-7390SLV 15.6"<br>
+            $3,199.99</br>
+	        50</br>
+            <a href="images/Alienware.jpg">Large</a> | <a href="#">Remove Product</a>
+        </p>
+    </td>
 </tr>
 <tr>
 	<td><img src="images/dell1.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-    <td class="td-element">Dell Inspiron 15 5000 Gaming (AMD) Laptop 15.6 inch"</br>
+    <td class="td-align-left">Dell Inspiron 15 5000 Gaming (AMD) Laptop 15.6 inch"</br>
     $899.99</br>
 	92</br>
         <a href="images/dell1.jpg">Large</a> |
@@ -46,11 +53,11 @@
 </tr>
 <tr>
 	<td><img src="images/msi1.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">MSI GL62M 7REX-1067 15.6"</br>
+	<td class="td-align-left">MSI GL62M 7REX-1067 15.6"</br>
 	$1,499.99</br>
 	84</br>
         <a href="images/msi1.jpg">Large</a> |
@@ -58,11 +65,11 @@
 </tr>
 <tr>
 	<td><img src="images/dell2.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">Dell Inspiron 15 7000 Gaming Series w/ Intel Core</br>
+	<td class="td-align-left">Dell Inspiron 15 7000 Gaming Series w/ Intel Core</br>
 	$1,599.99</br>
 	88</br>
         <a href="images/dell2.jpg">Large</a> |
@@ -70,11 +77,11 @@
 </tr>
 <tr>
 	<td><img src="images/msi2.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">MSI GS Series GS63VR Stealth Pro-422 15.6" Intel Core i7 6th Gen </br>
+	<td class="td-align-left">MSI GS Series GS63VR Stealth Pro-422 15.6" Intel Core i7 6th Gen </br>
 	$1,999.99</br>
 	78</br>
         <a href="images/msi12.jpg">Large</a> |
@@ -82,11 +89,11 @@
 </tr>
 <tr>
 	<td><img src="images/acer1.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">Acer Acer Aspire Nitro An515-51-58Lk 15.6"</br>
+	<td class="td-align-left">Acer Acer Aspire Nitro An515-51-58Lk 15.6"</br>
         $898.00</br>
 	42</br>
         <a href="images/acer1.jpg">Large</a> |
@@ -94,11 +101,11 @@
 </tr>
 <tr>
 	<td><img src="images/surface.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">Surface Laptop</br>
+	<td class="td-align-left">Surface Laptop</br>
 	$1,199.99</br>
 	35</br>
         <a href="images/surface.jpg">Large</a> |
@@ -106,11 +113,11 @@
 </tr>
 <tr>
 	<td><img src="images/asus1.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">ASUS ROG Zephyrus GX501VS-XS71 15.6"</br>
+	<td class="td-align-left">ASUS ROG Zephyrus GX501VS-XS71 15.6"</br>
 	$2,899.99</br>
 	37</br>
         <a href="images/asus1.jpg">Large</a> |
@@ -118,11 +125,11 @@
 </tr>
 <tr>
 	<td><img src="images/asus2.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">ASUS ROG GL753VD-DS71 17.3" Intel Core i7 7th Gen</br>
+	<td class="td-align-left">ASUS ROG GL753VD-DS71 17.3" Intel Core i7 7th Gen</br>
 	$1,204.94</br>
 	15</br>
         <a href="images/asus2.jpg">Large</a> |
@@ -130,11 +137,11 @@
 </tr>
 <tr>
 	<td><img src="images/asus3.jpg" class="picture1"></td>
-    <td class="td-element-right">Product:</br>
+    <td class="td-align-right">Product:</br>
         Price:</br>
         Quantity Instock:</br>
         &nbsp</td>
-	<td class="td-element">ASUS ROG GL502VS-Q72S-CB 15.6" Intel Core i7 6th Gen</br>
+	<td class="td-align-left">ASUS ROG GL502VS-Q72S-CB 15.6" Intel Core i7 6th Gen</br>
 	$2,599.00</br>
 	56</br>
         <a href="images/asus3.jpg">Large</a> |
