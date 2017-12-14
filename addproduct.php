d21b4db0d021e16b90260683618c910b8e4ba7d5
peter.robinson <peter.robinson.on@gmail.com>
2017-12-13 19:47:43 -0500

    add php dbClass

--- addproduct.php
+++ addproduct.php
@@ -1,12 +1,134 @@
 <?php
 
-#include "inc\DbConnection.php";
+//phpinfo();
+
+echo ini_get('display_errors');
+
+if (!ini_get('display_errors')) {
+    ini_set('display_errors', '1');
+}
+echo ini_get('display_errors');
+
+
+include "inc/DbConnection.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') //to check if there was a post.
 {
 
+   // load the values into variables: host, username, passwd, database
+    /*
+  $host="db-mysql.zenit";
+  $db_user="uli705_173a20";
+  $db_password="csHE3669";
+  $database="uli705_173a20";
+    */
 
-    $name = $_POST['name'];
+    $host="50.62.177.37";
+    $port="3306";
+    $db_user="senecauli";
+    $db_password="P@ssw0rd";
+    $database="smarttech";
+
+    // Connect to database server.
+    $con = mysqli_connect($host,$db_user,$db_password,$database,$port);
+
+        // Check connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }else
+    {
+        echo 'Connected successfully';
+
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $price = $_POST['price'];
+        $quantity = $_POST['quantity'];
+        $imagepath ="images/asus1.jpg"; // $_POST['imagepath'];
+
+        foreach($_POST  as $key => $value)
+        {
+             echo "$key -> $value";
+        }
+
+        $query = "INSERT INTO products(Name,Category,Price,Qty,ImagePath) values('$name','$category',$price,$quantity,'$imagepath')";
+
+        print "<br/>";
+        print $query;
+
+
+        $rowsaffected=mysqli_query($con,$query);
+
+        print "Rows Affected: $rowsaffected";
+
+        if ( !empty($rowsaffected))
+        {
+            echo "Added this record successfully!";
+
+        }else
+        {
+            echo "Inset Failed!";
+
+        }
+
+    }
+
+
+
+    /*getting the picture
+    if(isset($_FILES["file"]["type"]))
+    {
+        $validextensions = array("jpeg", "jpg", "png");
+        $temporary = explode(".", $_FILES["file"]["name"]);
+        $file_extension = end($temporary);
+
+        if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")) && in_array($file_extension, $validextensions)) {
+            if ($_FILES["file"]["error"] > 0)
+            {
+                echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
+            }
+            else
+            {
+                if (file_exists("images/" . $_FILES["file"]["name"])) {
+                    echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+                }
+                else
+                {
+                    $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
+                    $targetPath = "images/".$_FILES['file']['name']; // Target path where file is to be stored
+                    move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+
+                    foreach($_FILES as $key=>$val){
+                        // Here $val is also array like ["Hello World 1 A","Hello World 1 B"], and so on
+                        // And $key is index of $array array (ie,. 0, 1, ....)
+                        echo "key $key";
+
+                        foreach($val as $k=>$v){
+                            // $v is string. "Hello World 1 A", "Hello World 1 B", ......
+                            // And $k is $val array index (0, 1, ....)
+                            echo "value:$v . <br />";
+                        }
+                    }
+
+
+                    echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+                    echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+                    echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+                    echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                    echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+                }
+            }
+        }
+        else
+        {
+            echo "<span id='invalid'>***Invalid file Size or Type***<span>";
+        }
+    }
+*/
+
+
+/*
+$name = $_POST['name'];
     $category = $_POST['category'];
     $price = $_POST['price'];
     $quantity = $_POST['quantity'];
@@ -40,6 +162,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') //to check if there was a post.
 
     print "$var";
     echo "$rslt inserted Succesfull";
+*/
 }
 ?>
 
@@ -72,9 +195,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') //to check if there was a post.
         <section id="addform">
             <hr>
             <h2>Adding New Products</h2>
+
+            <?php
+
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') //to check if there was a post.
+            {
+                if($rowsaffected) {
+                    echo "<p class =\"success\">The product was succesfully inserted. $rowsaffected record added. </p>";
+                }else
+                {
+                    echo "<p class =\"error\">Adding product failed. </p>";
+                }
+            }
+            ?>
             <hr>
             <div id="updateproduct">
-                <form  id="addform" name="addform" method="POST" action="addproduct.php">
+                <form  id="addform" name="addform" method="POST" action="addproduct.php"  enctype="multipart/form-data">
                     <table>
                         <tr>
                             <td class="headertext">Name</td>
@@ -82,7 +218,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') //to check if there was a post.
                         </tr>
                         <tr>
                             <td class="headertext">Category</td>
-                            <td class="td-element"><input type="text" name="category" placeholder="eg. Laptop"/></td>
+                            <td class="td-element"><select name="category">
+                                    <option value="Laptop">Laptop</option>
+                                    <option value="Desktop">Desktop</option>
+                                    <option value="External">External Storage</option>
+                                    <option value="Flash Drives">Flash Drives</option>
+                                    <option value="HeadPhone">Headphone</option>
+                                    <option value="Monitor">Monitor</option>
+                                    <option value="Printers">Printers</option>
+                                    <option value="Projectors">Projectors</option>
+                                    <option value="Speakers">Speakers</option>
+                                    <option value="Tablets">Tablets</option>
+                                </select></td>
                         </tr>
                         <tr>
                             <td class="headertext">Price</td>
@@ -94,8 +241,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') //to check if there was a post.
                         </tr>
                         <tr>
                             <td class="headertext">Picture</td>
-                            <td class="td-element"><input type="button" value="Upload Picture"/><br/>
-                            <td class="td-element"><input type="text" name="imagepath" placeholder=""/>
+                            <td class="td-element"><input type="file" value="Upload Picture"/><br/>
                             </td>
                         </tr>
                         <tr>
--- inc/DbConnection.php
+++ inc/DbConnection.php
@@ -15,42 +15,63 @@ class DbConnection
     var $_dbh = null;
 
 
-
-    public function __construct($host, $db_user, $db_password, $database)
+/*
+    public function DbConnection($host, $db_user, $db_password, $database)
     {
         $this->_host = $host;
         $this->_db_user = $db_user;
         $this->_db_password = $db_password;
         $this->_database = $database;
         $this->_dbh = null;
+        echo "Host: $host";
     }
-/*
-    public function __construct()
-    {
+*/
 
+    public function DbConnection()
+    {
+        /*
         $this->_host = "db-mysql.zenit";
         $this->_db_user = "uli705_081a34";
         $this->_db_password = "11111111";
         $this->_database = "uli705_173a20";
         $this->_dbh = null;
+        */
+
+
+        $this->_host = "50.62.177.37";
+        $this->_db_user = "senecauli";
+        $this->_port = "3306";
+        $this->_db_password = "P@ssw0rd";
+        $this->_database = "smarttech";
+        $this->_dbh = null;
+
     }
-*/
+
     public function OpenConnection()
     {
-        $dbh = mysql_connect($this->_host, $this->_db_user, $this->_db_password);
-        if ($dbh) {
-            $this->_dbh = $dbh;
-            mysql_select_db($this->_database);
+
+        // Connect to database server.
+        $con = mysqli_connect($this->host, $this->db_user, $this->db_password, $this->database, $this->port);
+
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        } else {
+
+            $this->_dbh = $con;
         }
-        return $dbh;
+        return $this->dbh;
     }
+
+
 
     public function Select_Query($query)
     {
         $rows = null;
-        $result = mysql_query($query, $this->dbh);
+        $result =mysqli_query($this->_dbh,$query);
+
         if ($result) {
-            $rows = mysql_fetch_array($result);
+            $rows = mysqli_fetch_assoc($result);
             return $rows;
         }
         return null;
@@ -58,9 +79,10 @@ class DbConnection
     public function ExecuteUpdateInsertDelete($query)
     {
         $rows = null;
-        $rows_affect = mysql_query($query, $this->dbh);
+        $rows_affect = mysqli_query($query, $this->dbh);
         if ($rows_affect) {
-           return  $rows_affect;
+            //get rows affected
+           return  mysqli_affected_rows($this->dbh);
         }
         return null;
     }
